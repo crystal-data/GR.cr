c51bf1130b37fa6371052f29a70ebe281d7d6f83
Jun Makino <makino@localhost.localdomain>
2020-02-24 16:19:33 +0900

    gr3drotate.cr added

---
+++ gr3drotate.cr
@@ -0,0 +1,34 @@
+#
+# gr3drorate.cf
+#
+#  sample to make mp4 file 
+
+require "./grlib.cr"
+include Math
+include GR
+ENV["GKS_WSTYPE"]= "mp4"
+setwindow(0,1,0,1)
+setviewport(0.1, 0.9,0.1, 0.9)
+n=50
+dx = 1.0/n
+x=Array(Float64).new(n){|i| i*dx+dx/2}
+y=Array(Float64).new(n){|i| i*dx+dx/2}
+h=Array(Float64).new(11){|i| i*0.2-1}
+z = Array(Float64).new
+n.times{|i| 
+  n.times{|j|
+    # xd=x[i]-0.5
+    # yd=y[j]-0.5
+    # z.push exp(-(xd*xd+yd*yd)*10)
+    z.push (sin(x[i]*PI*2)*sin(y[j]*PI)+1)/2
+  }
+}
+
+90.times{|i|
+  clearws()
+  setspace(0,1,30, i)
+  axes3d(0.2, 0.2, 0.2, 0.0, 0.0, 0.0, 1,1,1,0.01)
+  surface(x,y,z, 4)
+  contour(x,y,h,z, 3)
+  updatews()
+}
