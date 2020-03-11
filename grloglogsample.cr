dfa909b3a0cce100f735dfaf084b67919433bcfa
Jun Makino <makino@localhost.localdomain>
2020-03-11 22:29:35 +0900

    grloglogsample.cr some comments added

--- grloglogsample.cr
+++ grloglogsample.cr
@@ -1,8 +1,11 @@
+#
+# sample for log-log plot
+#
 require "./grlib.cr"
 include GR
-setwindow(0.1,1,0.01,1)
-setscale(3)
-box(0.25,0.1, 0.1, 0.01,  xlog: true, ylog: true)
+setwindow(0.1,1,0.01,1)  # range should be in original values
+setscale(3)              # 1: log x, 2: log y (and thus 3: both)
+box(0.25,0.1, 0.1, 0.01,  xlog: true, ylog: true) #xlog and ylog controls axes
 n=21
 dx=1.0/(n-1)
 x=Array(Float64).new(n){|i| (i+1)*dx}
