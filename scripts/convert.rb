b0815aed443ae1206f55e7d384dd65226609dd7e
kojix2 <2xijok@gmail.com>
2021-04-12 07:29:47 +0900

    Improved converter

--- scripts/convert.rb
+++ scripts/convert.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#
+
 # This is a ruby script to convert a C header json file to crystal code.
 # Please use c2ffi https://github.com/rpav/c2ffi to generate json files
 # from C headers before running this script.
@@ -22,12 +22,15 @@ TYPE_TABLE = {
 }.freeze
 
 @unknown = []
+@error_flag = false
 
 def crystal_type(str, fname)
   type = TYPE_TABLE[str]
   if type.nil?
-    @unknown << [fname, str, caller.first]
-  end 
+    @unknown << [fname, str, caller.first] if type.nil?
+    @error_flag = true
+    type = ''
+  end
   type
 end
 
@@ -36,37 +39,40 @@ data = JSON.parse(File.read(ARGV[0]))
 # functions
 funcs = data.filter { |d| d['tag'] == 'function' }
 funcs.each do |f|
-  print 'fun '
-  print f['name']
-  print '('
+  str = String.new('')
+  str << 'fun '
+  str << f['name']
+  str << '('
   f['parameters'].each_with_index do |prm, idx|
-    idx.zero? || print(', ')
+    idx.zero? || str << (', ')
     if prm['type']['tag'] == ':pointer'
       if prm['type']['type']['tag'] == ':pointer'
-        print crystal_type(prm['type']['type']['type']['tag'], f['name'])
-        print '*'
+        str << crystal_type(prm['type']['type']['type']['tag'], f['name'])
+        str << '*'
       else
-        print crystal_type(prm['type']['type']['tag'], f['name'])
+        str << crystal_type(prm['type']['type']['tag'], f['name'])
       end
-      print '*'
+      str << '*'
     else
-      print crystal_type(prm['type']['tag'], f['name'])
+      str << crystal_type(prm['type']['tag'], f['name'])
     end
   end
-  print ') : '
+  str << ') : '
   if f['return-type']['tag'] == ':pointer'
-    print crystal_type(f['return-type']['type']['tag'], f[:name])
-    print '*'
+    str << crystal_type(f['return-type']['type']['tag'], f[:name])
+    str << '*'
   else
-    print crystal_type(f['return-type']['tag'], f['name'])
+    str << crystal_type(f['return-type']['tag'], f['name'])
   end
-  puts
+  print '# ' if @error_flag
+  @error_flag = false
+  puts str
 end
 
 puts
-puts "# Unknow types"
-puts "# #{@unknown.map{|i| i[1]}.uniq}"
-puts "# caller"
+puts '# Unknow types'
+puts "# #{@unknown.map { |i| i[1] }.uniq}"
+puts '# caller'
 @unknown.each do |i|
-  puts "# #{i.join(", ")}"
+  puts "# #{i.join(', ')}"
 end
