5943b346800d26864e5168352dce9de52d7430cb
kojix2 <2xijok@gmail.com>
2021-10-21 11:13:35 +0900

    fix variable name

--- examples/2darray.rb
+++ examples/2darray.rb
@@ -5,11 +5,11 @@ GR.setwindow(-0.5, 3.5, -0.5, 3.5)
 GR.axes(1, 1, -0.5, -0.5, 1, 1, -0.01)
 GR.setcolormap(1)
 
-a = Array(Int32).new(25) do |i|
+c = Array(Int32).new(25) do |i|
   (1000 + 255 * i / 24).to_i
 end
 
-GR.cellarray(-0.5, 3.5, -0.5, 3.5, 5, 5, a)
+GR.cellarray(-0.5, 3.5, -0.5, 3.5, 5, 5, c)
 GR.setviewport(0.825, 0.85, 0.2, 0.9)
 GR.colorbar
 GR.updatews
