818720da0c38ac44cbb41e90bbbae6128c772a92
kojix2 <2xijok@gmail.com>
2026-02-06 16:35:02 +0900

    Make float64 and int32 methods private; refactor int32_color method

--- src/gr_common/utils.cr
+++ src/gr_common/utils.cr
@@ -1,29 +1,30 @@
 module GRCommon
   module Utils
-    def float64(a)
-      Pointer(Float64).malloc(a.size) { |i| a[i] }
+    private def float64(a)
+      Pointer(Float64).malloc(a.size) { |i| a[i].to_f64 }
     end
 
+    private def int32(a)
+      Pointer(Int32).malloc(a.size) { |i| a[i].to_i32 }
+    end
+
+    private def int32_color(color)
+      if color.responds_to?(:flatten)
+        int32(color.flatten)
+      else
+        int32(color)
+      end
+    end
+
+    # Converts a String to a C string (null-terminated UInt8 pointer)
     def to_cchar(s)
       cp = Pointer(UInt8).malloc(s.size + 1)
       i = 0
       s.each_byte { |c| cp[i] = c; i += 1 }
-      #    (s.size+1).times{|i|p cp[i]}
       cp[s.size] = 0
       cp
     end
 
-    def inquiry_double(&block)
-      ptr = Pointer(Float64).malloc
-      yield(ptr)
-      ptr.value
-    end
-
-    def inquiry_int(&block)
-      ptr = Pointer(Int32).malloc
-      yield(ptr)
-      ptr.value
-    end
     # :nodoc:
     # Validate that all arrays have the same length.
     def ensure_same_length(*arrays)
